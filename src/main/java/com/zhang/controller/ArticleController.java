8c434c7e84773d87fdd14eea912686f00d76fb97
zhangwn <zhangwn@seaway.net.cn>
2019-04-11 16:05:19 +0800

    做完controller

---
+++ src/main/java/com/zhang/controller/ArticleController.java
@@ -0,0 +1,19 @@
+package com.zhang.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：zhangwn
+ * @date ：Created in 2019/4/11 14:03
+ * @modified By：
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("/article")
+public class ArticleController {
+    public static void main(String[] args){
+
+    }
+}
